3ac83f48af3a21281f8d1ce2751a3f8cfa45640d
LithiumSR <leonardosarra@outlook.com>
2018-03-01 15:56:15 +0100

    Common: Minor cleanup

--- common.h
+++ common.h
@@ -25,7 +25,6 @@
             do { if (DEBUG) fprintf(stderr, fmt, ##__VA_ARGS__); } while (0)
 
 /* Configuration parameters */
-#define MAX_CONN_QUEUE  3   // max number of connections the server can queue
 #define SERVER_ADDRESS  "127.0.0.1"
 #define SERVER_COMMAND  "QUIT"
 #define SERVER_PORT     8888
