04dbf862d0aeaef5276e5b88418547260e50413c
Leonardo Sarra <leonardosarra@outlook.com>
2018-03-10 22:49:45 +0100

    Linked_list: Removed unused code

--- linked_list.h
+++ linked_list.h
@@ -6,20 +6,6 @@
 typedef struct ListItem {
   struct ListItem* prev;
   struct ListItem* next;
-  float rotational_force;
-  float translational_force;
-  float x;
-  float prev_x;
-  float y;
-  float prev_y;
-  float theta;
-  Image * v_texture;
-  int id;
-  struct sockaddr_in user_addr;
-  time_t last_update_time;
-  time_t creation_time;
-  int isAddrReady;
-  int afk_counter;
 } ListItem;
 
 typedef struct ListHead {
