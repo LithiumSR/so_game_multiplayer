fd696ce8ba0b0bae975edabfa2c19beb255eb876
Leonardo Sarra <leonardosarra@outlook.com>
2018-04-06 21:54:30 +0200

    Game protocol: Updated ClientUpdate structure

--- so_game_protocol.h
+++ so_game_protocol.h
@@ -61,13 +61,12 @@ typedef struct {
 // id is the id of the vehicle
 typedef struct {
   int id;
-  int force_refresh;
   float x;
   float y;
   float theta;
   float rotational_force;
   float translational_force;
-  struct timeval client_update_time;
+  struct timeval client_update_time, client_creation_time;
 } ClientUpdate;
 
 // server world update, send by server (UDP)
