f68db9c2d1c247448d7be44039e39919d1052f30
Leonardo Sarra <leonardosarra@outlook.com>
2018-04-06 20:05:58 +0200

    Vehicle: Added time variable to vehicle structure

--- vehicle.h
+++ vehicle.h
@@ -4,6 +4,10 @@
 #include "linked_list.h"
 #include <semaphore.h>
 
+#if USE_VEHICLE_SEMAPHORE == 1
+    #define _USE_VEHICLE_SEM_
+#endif
+
 struct World;
 struct Vehicle;
 typedef void (*VehicleDtor)(struct Vehicle* v);
@@ -13,9 +17,10 @@ typedef struct Vehicle {
   int id;
   struct World* world;
   Image* texture;
+  #ifdef _USE_VEHICLE_SEM_
   sem_t vsem;
+  #endif
   pthread_mutex_t mutex;
-
   // these are the forces that will be applied after the update and the critical section
   float translational_force_update;
   float rotational_force_update;
@@ -33,7 +38,7 @@ typedef struct Vehicle {
   float mass, angular_mass;
   float rotational_force, max_rotational_force, min_rotational_force;
   float translational_force, max_translational_force, min_translational_force;
-
+  struct timeval world_update_time;
   int gl_texture;
   int gl_list;
   VehicleDtor _destructor;
@@ -43,15 +48,18 @@ void Vehicle_init(Vehicle* v, struct World* w, int id, Image* texture);
 
 void Vehicle_reset(Vehicle* v);
 
-void Vehicle_getXYTheta(Vehicle* v,float* x, float* y, float* theta);
-
-void Vehicle_setXYTheta(Vehicle* v, float x, float y, float theta);
-
-int Vehicle_update(Vehicle* v, float dt);
-
 void Vehicle_getForcesUpdate(Vehicle* v, float* translational_update, float* rotational_update);
+
+void Vehicle_getXYTheta(Vehicle* v,float* x, float* y, float* theta);
 
 void Vehicle_setForcesUpdate(Vehicle* v, float translational_update, float rotational_update);
 
+void Vehicle_setXYTheta(Vehicle* v, float x, float y, float theta);
+
+void Vehicle_getTime(Vehicle* v, struct timeval* time);
+
+void Vehicle_setTime(Vehicle* v, struct timeval time);
+
+int Vehicle_update(Vehicle* v, float dt);
 
 void Vehicle_destroy(Vehicle* v);
