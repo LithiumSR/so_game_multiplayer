b69a62f8881b2752c4bf0ef04fc328bfcc4faf22
Leonardo Sarra <leonardosarra@outlook.com>
2018-04-10 18:23:56 +0200

    Common: Added macros to control music settings

--- common.h
+++ common.h
@@ -25,8 +25,10 @@
 /* Configuration parameters */
 #define SERVER_ADDRESS  "127.0.0.1"
 #define USE_VEHICLE_SEMAPHORE 0 //Don't tinker with that
+#define BACKGROUND_TRACK 1
 #define WORLDSIZE 512
 #define DEBUG 1
+#define CLIENT_AUDIO 1
 #define SINGLEPLAYER 0
 #define BUFFERSIZE 1000000
 #define UDPPORT 8888
