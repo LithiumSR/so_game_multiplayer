4d83fa6ce79dcff95e879725cf4bd7f4ec828fee
Leonardo Sarra <leonardosarra@outlook.com>
2018-04-10 18:19:47 +0200

    Client_OP: Added getAudioContext()

--- client_op.c
+++ client_op.c
@@ -14,6 +14,8 @@
 #include "vehicle.h"
 #include "world_viewer.h"
 #include "so_game_protocol.h"
+#include "audio_context.h"
+#include <string.h>
 
 //Used to get ID from server
 int getID(int socket_desc){
@@ -151,7 +153,7 @@ Image* getTextureMap(int socket){
         msg_len+=ret;
     }
     ImagePacket* deserialized_packet = (ImagePacket*)Packet_deserialize(buf_rcv, msg_len+ph_len);
-    printf("[Texture Request] Ricevuto bytes %d \n",msg_len+ph_len);
+    printf("[Texture Request] Received %d bytes \n",msg_len+ph_len);
     Packet_free(&(request->header));
     Image* ris=deserialized_packet->image;
     free(deserialized_packet);
@@ -233,6 +235,60 @@ Image* getVehicleTexture(int socket,int id){
     Image* im=deserialized_packet->image;
     free(deserialized_packet);
     return im;
+}
+
+AudioContext* getAudioContext(int socket_desc){
+    char buf_send[BUFFERSIZE];
+    char buf_rcv[BUFFERSIZE];
+    AudioInfoPacket* request=(AudioInfoPacket*)malloc(sizeof(AudioInfoPacket));
+    PacketHeader ph;
+    ph.type=GetAudioInfo;
+    request->header=ph;
+    request->track_number=-1;
+    int size=Packet_serialize(buf_send,&(request->header));
+    if(size==-1) return NULL;
+    int bytes_sent=0;
+    int ret=0;
+    while(bytes_sent<size){
+        ret=send(socket_desc,buf_send+bytes_sent,size-bytes_sent,0);
+        if (ret==-1 && errno==EINTR) continue;
+        ERROR_HELPER(ret,"Can't send ID request");
+        if (ret==0) break;
+        bytes_sent+=ret;
+    }
+    Packet_free(&(request->header));
+    int ph_len=sizeof(PacketHeader);
+    int msg_len=0;
+    while(msg_len<ph_len){
+        ret=recv(socket_desc, buf_rcv+msg_len, ph_len-msg_len, 0);
+        if (ret==-1 && errno == EINTR) continue;
+        ERROR_HELPER(msg_len, "Cannot read from socket");
+        msg_len+=ret;
+        }
+    PacketHeader* header=(PacketHeader*)buf_rcv;
+    size=header->size-ph_len;
+    msg_len=0;
+    while(msg_len<size){
+        ret=recv(socket_desc, buf_rcv+msg_len+ph_len, size-msg_len, 0);
+        if (ret==-1 && errno == EINTR) continue;
+        ERROR_HELPER(msg_len, "Cannot read from socket");
+        msg_len+=ret;
+        }
+    AudioInfoPacket* deserialized_packet = (AudioInfoPacket*)Packet_deserialize(buf_rcv, msg_len+ph_len);
+    printf("[Get Id] Received %dbytes \n",msg_len+ph_len);
+    int track_number=deserialized_packet->track_number;
+    Packet_free(&(deserialized_packet->header));
+    char number[12];
+	sprintf(number, "%d", track_number);
+	char filename[128];
+	strcpy(filename, "./sounds/track");
+	strcat(filename,number);
+	strcat(filename,".wav");
+    AudioContext_openDevice();
+    AudioContext* ac=(AudioContext*)malloc(sizeof(AudioContext));
+    AudioContext_init(ac,filename);
+    AudioContext_startTrackLoop(ac);
+    return ac;
 }
 
 int sendGoodbye(int socket,int id){
