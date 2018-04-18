a5655749e6a6ee97f6f587ede043d0b39d24af8f
Leonardo Sarra <leonardosarra@outlook.com>
2018-04-18 23:55:26 +0200

    WorldViewer: Reworked audio handling and WorldViewer_exit()

--- av_framework/world_viewer.h
+++ av_framework/world_viewer.h
@@ -1,6 +1,6 @@
 #pragma once
-#include "audio_context.h"
 #include "../game_framework/vehicle.h"
+#include "audio_context.h"
 #include "world.h"
 // call this to start the visualization of the stuff.
 // This will block the program, and terminate when pressing esc on the viewport
@@ -10,3 +10,12 @@ void WorldViewer_runGlobal(World *world, Vehicle *self, AudioContext *ac,
 // Use this if you want to correctly kill the client's path of execution, glut
 // loop and the viewer window
 void WorldViewer_exit(int ret);
+
+// Add a sound/track to the GUI
+void WorldViewer_addAudioTrack(AudioContext *ac);
+
+// Create GUI's AudioList
+void WorldViewer_createAudio(void);
+
+// Destroy GUI's AudioList
+void WorldViewer_destroyAudio(void);
