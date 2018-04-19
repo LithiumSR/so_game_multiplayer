b67be83af6f1be00d4ca68e2e3937da44e6b1741
Leonardo Sarra <leonardosarra@outlook.com>
2018-04-19 13:25:28 +0200

    WorldViewer: Take advantage of AudioList_cleanExpiredItem()

--- av_framework/world_viewer.c
+++ av_framework/world_viewer.c
@@ -82,16 +82,16 @@ void keyPressed(unsigned char key, int x, int y) {
 void specialInput(int key, int x, int y) {
   switch (key) {
     case GLUT_KEY_UP:
-      viewer.self->translational_force_update += 0.1;
+      Vehicle_increaseTranslationalForce(viewer.self,0.1);
       break;
     case GLUT_KEY_DOWN:
-      viewer.self->translational_force_update -= 0.1;
+      Vehicle_decreaseTranslationalForce(viewer.self,0.1);
       break;
     case GLUT_KEY_LEFT:
-      viewer.self->rotational_force_update += 0.1;
+      Vehicle_increaseRotationalForce(viewer.self,0.1);
       break;
     case GLUT_KEY_RIGHT:
-      viewer.self->rotational_force_update -= 0.1;
+      Vehicle_decreaseRotationalForce(viewer.self,0.1);
       break;
     case GLUT_KEY_PAGE_UP:
       viewer.camera_z += 0.1;
@@ -112,11 +112,10 @@ void idle(void) {
   World_update(viewer.world);
   usleep(30000);
   if (destroy) _WorldViewer_exit();
+  pthread_mutex_lock(&audio_list_mutex);
+  AudioList_cleanExpiredItem(audio_list);
+  pthread_mutex_unlock(&audio_list_mutex);
   glutPostRedisplay();
-
-  // decay the commands
-  viewer.self->translational_force_update *= 0.999;
-  viewer.self->rotational_force_update *= 0.7;
 }
 
 void Surface_destructor(Surface *s) {
