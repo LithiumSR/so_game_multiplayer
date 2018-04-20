9daad2f5ea4139bb6b3ecb5e238bfc0d2c919ad6
Leonardo Sarra <leonardosarra@outlook.com>
2018-04-20 17:08:42 +0200

    World: Prevent collisions between vehicles

--- av_framework/world.c
+++ av_framework/world.c
@@ -64,6 +64,22 @@ void World_update(World* w) {
     Vehicle_decayForcesUpdate(v, tr_decay, rt_decay);
     if (!Vehicle_update(v, delta * w->time_scale)) {
       Vehicle_reset(v);
+    } else {
+      ListItem* item2 = w->vehicles.first;
+      char flag = 0;
+      while (item2) {
+        Vehicle* v2 = (Vehicle*)item2;
+        if (v2 == v) goto END;
+        pthread_mutex_lock(&v2->mutex);
+        Vehicle_fixCollisions(v,v2);
+        pthread_mutex_unlock(&v2->mutex);
+        END: item2 = item2->next;
+      }
+      if (!flag) {
+        v->is_new = 0;
+        v->temp_x = v->x;
+        v->temp_y = v->y;
+      }
     }
     Vehicle_setTime(v, current_time);
     pthread_mutex_unlock(&v->mutex);
