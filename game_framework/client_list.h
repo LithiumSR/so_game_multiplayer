afad1e43ce902bf568d0dd7449a085cdff292638
Leonardo Sarra <leonardosarra@outlook.com>
2018-04-21 21:18:11 +0200

    ClientList: Added username to structure

--- game_framework/client_list.h
+++ game_framework/client_list.h
@@ -2,18 +2,20 @@
 #include <netinet/in.h>
 #include <time.h>
 #include "../av_framework/image.h"
-
+#include "../common/common.h"
 typedef struct ClientListItem {
   struct ClientListItem *next;
   float rotational_force;
   float translational_force;
   float x, y, theta;
+  char username[USERNAME_LEN];
   float prev_x, prev_y;
   float x_shift, y_shift;
   Image *v_texture;
   int id;
   struct sockaddr_in user_addr_udp, user_addr_tcp;
   struct timeval last_update_time, creation_time;
+  char inside_chat;
   int is_udp_addr_ready;
   int afk_counter;
   int force_refresh;
