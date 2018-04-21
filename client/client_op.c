6540af1996dfe4eb37658cfb80cc9d4bc952080f
Leonardo Sarra <leonardosarra@outlook.com>
2018-04-21 21:17:59 +0200

    Client_OP: Added joinChat()

--- client/client_op.c
+++ client/client_op.c
@@ -7,7 +7,6 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include "../av_framework/audio_context.h"
 #include "../av_framework/image.h"
 #include "../av_framework/surface.h"
 #include "../av_framework/world.h"
@@ -16,13 +15,11 @@
 #include "../game_framework/so_game_protocol.h"
 #include "../game_framework/vehicle.h"
 
-char sent_goodbye = 0;
-
 // Used to get ID from server
 int getID(int socket_desc) {
   char buf_send[BUFFERSIZE];
   char buf_rcv[BUFFERSIZE];
-  IdPacket *request = (IdPacket *)malloc(sizeof(IdPacket));
+  IdPacket* request = (IdPacket*)malloc(sizeof(IdPacket));
   PacketHeader ph;
   ph.type = GetId;
   request->header = ph;
@@ -47,7 +44,7 @@ int getID(int socket_desc) {
     ERROR_HELPER(msg_len, "Cannot read from socket");
     msg_len += ret;
   }
-  PacketHeader *header = (PacketHeader *)buf_rcv;
+  PacketHeader* header = (PacketHeader*)buf_rcv;
   size = header->size - ph_len;
 
   msg_len = 0;
@@ -57,18 +54,18 @@ int getID(int socket_desc) {
     ERROR_HELPER(msg_len, "Cannot read from socket");
     msg_len += ret;
   }
-  IdPacket *deserialized_packet =
-      (IdPacket *)Packet_deserialize(buf_rcv, msg_len + ph_len);
+  IdPacket* deserialized_packet =
+      (IdPacket*)Packet_deserialize(buf_rcv, msg_len + ph_len);
   debug_print("[Get Id] Received %dbytes \n", msg_len + ph_len);
   int id = deserialized_packet->id;
   Packet_free(&(deserialized_packet->header));
   return id;
 }
 
-Image *getElevationMap(int socket) {
+Image* getElevationMap(int socket) {
   char buf_send[BUFFERSIZE];
   char buf_rcv[BUFFERSIZE];
-  ImagePacket *request = (ImagePacket *)malloc(sizeof(ImagePacket));
+  ImagePacket* request = (ImagePacket*)malloc(sizeof(ImagePacket));
   PacketHeader ph;
   ph.type = GetElevation;
   request->header = ph;
@@ -96,7 +93,7 @@ Image *getElevationMap(int socket) {
     msg_len += ret;
   }
 
-  PacketHeader *incoming_pckt = (PacketHeader *)buf_rcv;
+  PacketHeader* incoming_pckt = (PacketHeader*)buf_rcv;
   size = incoming_pckt->size - ph_len;
   msg_len = 0;
   while (msg_len < size) {
@@ -106,19 +103,19 @@ Image *getElevationMap(int socket) {
     msg_len += ret;
   }
 
-  ImagePacket *deserialized_packet =
-      (ImagePacket *)Packet_deserialize(buf_rcv, msg_len + ph_len);
+  ImagePacket* deserialized_packet =
+      (ImagePacket*)Packet_deserialize(buf_rcv, msg_len + ph_len);
   debug_print("[Elevation request] Received %d bytes \n", msg_len + ph_len);
   Packet_free(&(request->header));
-  Image *ris = deserialized_packet->image;
+  Image* ris = deserialized_packet->image;
   free(deserialized_packet);
   return ris;
 }
 
-Image *getTextureMap(int socket) {
+Image* getTextureMap(int socket) {
   char buf_send[BUFFERSIZE];
   char buf_rcv[BUFFERSIZE];
-  ImagePacket *request = (ImagePacket *)malloc(sizeof(ImagePacket));
+  ImagePacket* request = (ImagePacket*)malloc(sizeof(ImagePacket));
   PacketHeader ph;
   ph.type = GetTexture;
   request->header = ph;
@@ -143,7 +140,7 @@ Image *getTextureMap(int socket) {
     ERROR_HELPER(ret, "Cannot read from socket");
     msg_len += ret;
   }
-  PacketHeader *incoming_pckt = (PacketHeader *)buf_rcv;
+  PacketHeader* incoming_pckt = (PacketHeader*)buf_rcv;
   size = incoming_pckt->size - ph_len;
   debug_print("[Texture Request] Size da leggere %d \n", size);
   msg_len = 0;
@@ -153,18 +150,18 @@ Image *getTextureMap(int socket) {
     ERROR_HELPER(ret, "Cannot read from socket");
     msg_len += ret;
   }
-  ImagePacket *deserialized_packet =
-      (ImagePacket *)Packet_deserialize(buf_rcv, msg_len + ph_len);
-  debug_print("[Texture Request] Received %d bytes \n", msg_len + ph_len);
+  ImagePacket* deserialized_packet =
+      (ImagePacket*)Packet_deserialize(buf_rcv, msg_len + ph_len);
+  debug_print("[Texture Request] Ricevuto bytes %d \n", msg_len + ph_len);
   Packet_free(&(request->header));
-  Image *ris = deserialized_packet->image;
+  Image* ris = deserialized_packet->image;
   free(deserialized_packet);
   return ris;
 }
 
-int sendVehicleTexture(int socket, Image *texture, int id) {
+int sendVehicleTexture(int socket, Image* texture, int id) {
   char buf_send[BUFFERSIZE];
-  ImagePacket *request = (ImagePacket *)malloc(sizeof(ImagePacket));
+  ImagePacket* request = (ImagePacket*)malloc(sizeof(ImagePacket));
   PacketHeader ph;
   ph.type = PostTexture;
   request->header = ph;
@@ -186,10 +183,10 @@ int sendVehicleTexture(int socket, Image *texture, int id) {
   return 0;
 }
 
-Image *getVehicleTexture(int socket, int id) {
+Image* getVehicleTexture(int socket, int id) {
   char buf_send[BUFFERSIZE];
   char buf_rcv[BUFFERSIZE];
-  ImagePacket *request = (ImagePacket *)malloc(sizeof(ImagePacket));
+  ImagePacket* request = (ImagePacket*)malloc(sizeof(ImagePacket));
   PacketHeader ph;
   ph.type = GetTexture;
   request->header = ph;
@@ -215,7 +212,7 @@ Image *getVehicleTexture(int socket, int id) {
     ERROR_HELPER(msg_len, "Cannot read from socket");
     msg_len += ret;
   }
-  PacketHeader *header = (PacketHeader *)buf_rcv;
+  PacketHeader* header = (PacketHeader*)buf_rcv;
   size = header->size - ph_len;
   char flag = 0;
   if (header->type == PostDisconnect) flag = 1;
@@ -228,23 +225,22 @@ Image *getVehicleTexture(int socket, int id) {
   }
 
   if (flag) {
-    IdPacket *packet =
-        (IdPacket *)Packet_deserialize(buf_rcv, msg_len + ph_len);
+    IdPacket* packet = (IdPacket*)Packet_deserialize(buf_rcv, msg_len + ph_len);
     Packet_free(&packet->header);
     return NULL;
   }
-  ImagePacket *deserialized_packet =
-      (ImagePacket *)Packet_deserialize(buf_rcv, msg_len + ph_len);
+  ImagePacket* deserialized_packet =
+      (ImagePacket*)Packet_deserialize(buf_rcv, msg_len + ph_len);
   debug_print("[Get Vehicle Texture] Received %d bytes \n", msg_len + ph_len);
-  Image *im = deserialized_packet->image;
+  Image* im = deserialized_packet->image;
   free(deserialized_packet);
   return im;
 }
 
-AudioContext *getAudioContext(int socket_desc) {
+AudioContext* getAudioContext(int socket_desc) {
   char buf_send[BUFFERSIZE];
   char buf_rcv[BUFFERSIZE];
-  AudioInfoPacket *request = (AudioInfoPacket *)malloc(sizeof(AudioInfoPacket));
+  AudioInfoPacket* request = (AudioInfoPacket*)malloc(sizeof(AudioInfoPacket));
   PacketHeader ph;
   ph.type = GetAudioInfo;
   request->header = ph;
@@ -269,7 +265,7 @@ AudioContext *getAudioContext(int socket_desc) {
     ERROR_HELPER(msg_len, "Cannot read from socket");
     msg_len += ret;
   }
-  PacketHeader *header = (PacketHeader *)buf_rcv;
+  PacketHeader* header = (PacketHeader*)buf_rcv;
   size = header->size - ph_len;
   msg_len = 0;
   while (msg_len < size) {
@@ -278,34 +274,29 @@ AudioContext *getAudioContext(int socket_desc) {
     ERROR_HELPER(msg_len, "Cannot read from socket");
     msg_len += ret;
   }
-  AudioInfoPacket *deserialized_packet =
-      (AudioInfoPacket *)Packet_deserialize(buf_rcv, msg_len + ph_len);
+  AudioInfoPacket* deserialized_packet =
+      (AudioInfoPacket*)Packet_deserialize(buf_rcv, msg_len + ph_len);
   debug_print("[Get Id] Received %dbytes \n", msg_len + ph_len);
   int track_number = deserialized_packet->track_number;
   char loop = deserialized_packet->loop;
-  MusicType type = deserialized_packet->type;
   Packet_free(&(deserialized_packet->header));
   char number[12];
   if (track_number > 1000) return NULL;
   sprintf(number, "%d", track_number);
   char filename[128];
-  if (type == Track)
-    strcpy(filename, "./resources/sounds/track");
-  else if (type == Effect)
-    strcpy(filename, "./resources/sounds/effect");
+  strcpy(filename, "./resources/sounds/track");
   strcat(filename, number);
   strcat(filename, ".wav");
   fprintf(stderr, "[GetAudioContext] Loading %s file...\n", filename);
   AudioContext_openDevice();
-  AudioContext *ac = (AudioContext *)malloc(sizeof(AudioContext));
+  AudioContext* ac = (AudioContext*)malloc(sizeof(AudioContext));
   AudioContext_init(ac, filename, loop);
   return ac;
 }
 
-int sendGoodbye(int socket, int socket_udp, int id, int messaging_enabled,
-                char *username, struct sockaddr_in server_addr) {
+int sendGoodbye(int socket, int id) {
   char buf_send[BUFFERSIZE];
-  IdPacket *idpckt = (IdPacket *)malloc(sizeof(IdPacket));
+  IdPacket* idpckt = (IdPacket*)malloc(sizeof(IdPacket));
   PacketHeader ph;
   ph.type = PostDisconnect;
   idpckt->id = id;
@@ -320,24 +311,53 @@ int sendGoodbye(int socket, int socket_udp, int id, int messaging_enabled,
     if (ret == 0) break;
     msg_len += ret;
   }
-  if (!messaging_enabled || socket_udp == -1 || sent_goodbye) return 0;
-  // send goodbye message
-  socklen_t serverlen = sizeof(server_addr);
-  PacketHeader goodbye_header;
-  goodbye_header.type = ChatMessage;
-  MessagePacket *goodbye_message =
-      (MessagePacket *)malloc(sizeof(MessagePacket));
-  goodbye_message->header = goodbye_header;
-  goodbye_message->message.id = id;
-  strncpy(goodbye_message->message.sender, username, USERNAME_LEN);
-  goodbye_message->message.type = Goodbye;
-  size = Packet_serialize(buf_send, &(goodbye_message->header));
-  if (size > 0)
-    sendto(socket_udp, buf_send, size, 0, (const struct sockaddr *)&server_addr,
-           serverlen);
-  Packet_free(&goodbye_message->header);
-
   debug_print("[Goodbye] Goodbye was successfully sent %d \n", msg_len);
-  sent_goodbye = 1;
   return 0;
+}
+
+int joinChat(int socket_desc, int id, char* username) {
+  char buf_send[BUFFERSIZE];
+  char buf_rcv[BUFFERSIZE];
+  MessagePacket* mp = (MessagePacket*)malloc(sizeof(MessagePacket));
+  PacketHeader ph;
+  ph.type = ChatMessage;
+  mp->message.id = id;
+  mp->header = ph;
+  strncpy(mp->message.sender, username, USERNAME_LEN);
+  mp->message.type = Hello;
+  int size = Packet_serialize(buf_send, &(mp->header));
+  int msg_len = 0;
+  while (msg_len < size) {
+    int ret = send(socket_desc, buf_send + msg_len, size - msg_len, 0);
+    if (ret == -1 && errno == EINTR) continue;
+    ERROR_HELPER(ret, "Can't send joinChat");
+    if (ret == 0) break;
+    msg_len += ret;
+  }
+
+  int ph_len = sizeof(PacketHeader);
+  msg_len = 0;
+  int ret=0;
+  while (msg_len < ph_len) {
+    ret = recv(socket_desc, buf_rcv + msg_len, ph_len - msg_len, 0);
+    if (ret == -1 && errno == EINTR) continue;
+    ERROR_HELPER(msg_len, "Cannot read from socket");
+    msg_len += ret;
+  }
+  PacketHeader* header = (PacketHeader*)buf_rcv;
+  size = header->size - ph_len;
+  msg_len = 0;
+  while (msg_len < size) {
+    ret = recv(socket_desc, buf_rcv + msg_len + ph_len, size - msg_len, 0);
+    if (ret == -1 && errno == EINTR) continue;
+    ERROR_HELPER(msg_len, "Cannot read from socket");
+    msg_len += ret;
+  }
+  IdPacket* deserialized_packet =
+      (IdPacket*)Packet_deserialize(buf_rcv, msg_len + ph_len);
+  int id_received= deserialized_packet->id;
+  Packet_free(&deserialized_packet->header);
+  Packet_free(&mp->header);
+  if(id==id_received) return 1;
+  else return 0;
 }
