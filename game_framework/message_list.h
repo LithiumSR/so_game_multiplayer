eb1ce1c4b2ffbf17d6c35faef416c7355f0ab733
Leonardo Sarra <leonardosarra@outlook.com>
2018-04-21 18:00:40 +0200

    MessageList: Added

---
+++ game_framework/message_list.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <netinet/in.h>
+#include <time.h>
+#include "../av_framework/image.h"
+#include "so_game_protocol.h"
+#include "vehicle.h"
+typedef struct MessageListItem {
+  struct MessageListItem* next;
+  int id;
+  char text[256];
+  char sender[32];
+  time_t time;
+  MessageType type;
+} MessageListItem;
+
+typedef struct MessageListHead {
+  MessageListItem* first;
+  MessageListItem* last;
+  int size;
+} MessageListHead;
+
+void MessageList_init(MessageListHead* head);
+MessageListItem* MessageList_find(MessageListHead* head, MessageListItem* item);
+MessageListItem* MessageList_insert(MessageListHead* head,
+                                    MessageListItem* item);
+MessageListItem* MessageList_detach(MessageListHead* head,
+                                    MessageListItem* item);
+void MessageList_destroy(MessageListHead* head);
+void MessageList_print(MessageListHead* users);
+void MessageList_removeAll(MessageListHead* users);
