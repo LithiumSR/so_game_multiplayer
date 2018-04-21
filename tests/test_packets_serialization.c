9d2b580654c269810a52044a9beca336d588caad
Leonardo Sarra <leonardosarra@outlook.com>
2018-04-21 14:26:29 +0200

    Tests: Minor change to the serialization test

--- tests/test_packets_serialization.c
+++ tests/test_packets_serialization.c
@@ -95,12 +95,12 @@ int main(int argc, char const* argv[]) {
   w_head.type = WorldUpdate;
 
   world_packet->header = w_head;
-  world_packet->num_vehicles = 1;
+  world_packet->num_update_vehicles = 1;
   world_packet->updates = update_block;
 
   printf("world_packet with:\ntype\t%d\nsize\t%d\nnum_v\t%d\n",
          world_packet->header.type, world_packet->header.size,
-         world_packet->num_vehicles);
+         world_packet->num_update_vehicles);
   printf("update_block:\nid\t\t%d\n(x,y,theta)\t(%f,%f,%f)\n",
          world_packet->updates->id, world_packet->updates->x,
          world_packet->updates->y, world_packet->updates->theta);
@@ -117,7 +117,7 @@ int main(int argc, char const* argv[]) {
   printf("deserialized packet with:\ntype\t%d\nsize\t%d\nnum_v\t%d\n",
          deserialized_wu_packet->header.type,
          deserialized_wu_packet->header.size,
-         deserialized_wu_packet->num_vehicles);
+         deserialized_wu_packet->num_update_vehicles);
   printf("update_block:\nid\t\t%d\n(x,y,theta)\t(%f,%f,%f)\n",
          deserialized_wu_packet->updates->id,
          deserialized_wu_packet->updates->x, deserialized_wu_packet->updates->y,
