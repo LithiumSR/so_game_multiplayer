f5c2efe6015ce03509c5473aeccbd1ac25aed873
Leonardo Sarra <leonardosarra@outlook.com>
2018-04-22 01:34:26 +0200

    WorldViewer: Minor cleanup

--- av_framework/world_viewer.c
+++ av_framework/world_viewer.c
@@ -126,7 +126,6 @@ void idle(void) {
     AudioList_cleanExpiredItem(audio_list);
     pthread_mutex_unlock(&audio_list_mutex);
   }
-  //Vehicle_decayForcesUpdate(viewer.self, 0.999, 0.7);
   glutPostRedisplay();
 }
 
