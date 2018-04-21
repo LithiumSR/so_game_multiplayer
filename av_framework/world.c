923ef0465a3f05094d3654268424feb1dcae05d2
Leonardo Sarra <leonardosarra@outlook.com>
2018-04-21 14:07:32 +0200

    World: Minor changes to update()

--- av_framework/world.c
+++ av_framework/world.c
@@ -46,22 +46,15 @@ int World_init(World* w, Image* surface_elevation, Image* surface_texture,
 void World_update(World* w) {
   struct timeval current_time;
   gettimeofday(&current_time, 0);
-
   struct timeval dt;
   timersub(&current_time, &w->last_update, &dt);
   float delta = dt.tv_sec + 1e-6 * dt.tv_usec;
-  float exp = delta / (30 * 1000);
-  float tr_decay = powf(1 - 0.001, exp);
-  float rt_decay = powf(1 - 0.3, exp);
-  if (tr_decay > 0.999) tr_decay = 0.999;
-  if (rt_decay > 0.7) rt_decay = 0.7;
   sem_t sem = w->vehicles.sem;
   sem_wait(&sem);
   ListItem* item = w->vehicles.first;
   while (item) {
     Vehicle* v = (Vehicle*)item;
     pthread_mutex_lock(&v->mutex);
-    Vehicle_decayForcesUpdate(v, tr_decay, rt_decay);
     if (!Vehicle_update(v, delta * w->time_scale)) {
       Vehicle_reset(v);
     } else {
