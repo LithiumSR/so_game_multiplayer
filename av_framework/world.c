1930e29d29bddb963b4bbd38b805799f48cbd65b
Leonardo Sarra <leonardosarra@outlook.com>
2018-04-26 23:14:16 +0200

    World: Improved decay simulation

--- av_framework/world.c
+++ av_framework/world.c
@@ -50,18 +50,15 @@ void World_update(World* w) {
   struct timeval dt;
   timersub(&current_time, &w->last_update, &dt);
   float delta = dt.tv_sec + 1e-6 * dt.tv_usec;
-  float exp = delta / (300000*1e-6);
+  float exp = delta / (30000*1e-6);
   float tr_decay = powf(1 - 0.001, exp);
   float rt_decay = powf(1 - 0.15, exp);
-  if (tr_decay > 0.999) tr_decay = 0.999;
-  if (rt_decay > 0.85) rt_decay = 0.85;
   sem_t sem = w->vehicles.sem;
   sem_wait(&sem);
   ListItem* item = w->vehicles.first;
   while (item) {
     Vehicle* v = (Vehicle*)item;
     pthread_mutex_lock(&v->mutex);
-    Vehicle_decayForcesUpdate(v, tr_decay, rt_decay);
     if (!Vehicle_update(v, delta * w->time_scale)) {
       Vehicle_reset(v);
     } else {
@@ -81,6 +78,7 @@ void World_update(World* w) {
         v->temp_x = v->x;
         v->temp_y = v->y;
       }
+      Vehicle_decayForcesUpdate(v, tr_decay, rt_decay);
     }
     Vehicle_setTime(v, current_time);
     pthread_mutex_unlock(&v->mutex);
@@ -96,11 +94,9 @@ void World_manualUpdate(World* w, Vehicle* v, struct timeval update_time) {
   struct timeval dt;
   timersub(&current_time, &update_time, &dt);
   float delta = dt.tv_sec + 1e-6 * dt.tv_usec;
-  float exp = delta / (300000*1e-6);
+  float exp = delta / (30000*1e-6);
   float tr_decay = powf(1 - 0.001, exp);
   float rt_decay = powf(1 - 0.15, exp);
-  if (tr_decay > 0.999) tr_decay = 0.999;
-  if (rt_decay > 0.85) rt_decay = 0.85;
   Vehicle_decayForcesUpdate(v, tr_decay, rt_decay);
   if (!Vehicle_update(v, delta * w->time_scale)) Vehicle_reset(v);
   Vehicle_setTime(v, current_time);
