c33715ca10a458e38e668cd3a9ac53074c8adec7
Leonardo Sarra <leonardosarra@outlook.com>
2018-04-26 23:12:00 +0200

    World: Improved decay simulation

--- av_framework/world.c
+++ av_framework/world.c
@@ -51,7 +51,7 @@ void World_update(World* w) {
   struct timeval dt;
   timersub(&current_time, &w->last_update, &dt);
   float delta = dt.tv_sec + 1e-6 * dt.tv_usec;
-    float exp = delta / (300000*1e-6);
+  float exp = delta / (30000 * 1e-6);
   float tr_decay = powf(1 - 0.001, exp);
   float rt_decay = powf(1 - 0.25, exp);
   if (tr_decay > 0.999) tr_decay = 0.999;
@@ -62,7 +62,6 @@ void World_update(World* w) {
   while (item) {
     Vehicle* v = (Vehicle*)item;
     pthread_mutex_lock(&v->mutex);
-    Vehicle_decayForcesUpdate(v,tr_decay,rt_decay);
     if (!Vehicle_update(v, delta * w->time_scale)) {
       Vehicle_reset(v);
     } else {
@@ -72,15 +71,17 @@ void World_update(World* w) {
         Vehicle* v2 = (Vehicle*)item2;
         if (v2 == v) goto END;
         pthread_mutex_lock(&v2->mutex);
-        Vehicle_fixCollisions(v,v2);
+        Vehicle_fixCollisions(v, v2);
         pthread_mutex_unlock(&v2->mutex);
-        END: item2 = item2->next;
+      END:
+        item2 = item2->next;
       }
       if (!flag) {
         v->is_new = 0;
         v->temp_x = v->x;
         v->temp_y = v->y;
       }
+      Vehicle_decayForcesUpdate(v, tr_decay, rt_decay);
     }
     Vehicle_setTime(v, current_time);
     pthread_mutex_unlock(&v->mutex);
@@ -96,14 +97,12 @@ void World_manualUpdate(World* w, Vehicle* v, struct timeval update_time) {
   struct timeval dt;
   timersub(&current_time, &update_time, &dt);
   float delta = dt.tv_sec + 1e-6 * dt.tv_usec;
-  float exp = delta / (300000*1e-6);
+  float exp = delta / (30000 * 1e-6);
   float tr_decay = powf(1 - 0.001, exp);
   float rt_decay = powf(1 - 0.25, exp);
-  if (tr_decay > 0.999) tr_decay = 0.999;
-  if (rt_decay > 0.7) rt_decay = 0.75;
   sem_t sem = w->vehicles.sem;
   sem_wait(&sem);
-  Vehicle_decayForcesUpdate(v,tr_decay,rt_decay);
+  Vehicle_decayForcesUpdate(v, tr_decay, rt_decay);
   if (!Vehicle_update(v, delta * w->time_scale)) Vehicle_reset(v);
   Vehicle_setTime(v, current_time);
   sem_post(&sem);
