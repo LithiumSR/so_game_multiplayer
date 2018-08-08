7fc4ae3777a298ba09750ff8cdd9049df77f7409
Leonardo Sarra <leonardosarra@outlook.com>
2018-08-08 22:27:47 +0200

    World: Improved collisions check accuracy

--- av_framework/world.h
+++ av_framework/world.h
@@ -1,9 +1,9 @@
 #pragma once
 #include <sys/time.h>
-#include "image.h"
 #include "../game_framework/linked_list.h"
-#include "surface.h"
 #include "../game_framework/vehicle.h"
+#include "image.h"
+#include "surface.h"
 
 typedef struct World {
   ListHead vehicles;  // list of vehicles
@@ -13,19 +13,24 @@ typedef struct World {
   float dt;
   struct timeval last_update;
   float time_scale;
+  char disable_collisions;
 } World;
 
-int World_init(World *w, Image *surface_elevation, Image *surface_texture,
+int World_init(World* w, Image* surface_elevation, Image* surface_texture,
                float x_step, float y_step, float z_step);
 
-void World_destroy(World *w);
+void World_destroy(World* w);
 
-void World_update(World *w);
+void World_update(World* w);
 
-Vehicle *World_getVehicle(World *w, int vehicle_id);
+void World_decayUpdate(World* w);
 
-Vehicle *World_addVehicle(World *w, Vehicle *v);
+Vehicle* World_getVehicle(World* w, int vehicle_id);
 
-Vehicle *World_detachVehicle(World *w, Vehicle *v);
+Vehicle* World_addVehicle(World* w, Vehicle* v);
 
-void World_manualUpdate(World *w, Vehicle *v, struct timeval update_time);
+Vehicle* World_detachVehicle(World* w, Vehicle* v);
+
+void World_manualUpdate(World* w, Vehicle* v, struct timeval update_time);
+
+void World_disableVehicleCollisions(World* w);
