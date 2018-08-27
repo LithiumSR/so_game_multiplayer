fce7f08df49fb329e55ae011c11f626c7a800d35
Leonardo Sarra <leonardosarra@outlook.com>
2018-08-27 18:43:42 +0200

    Server: Minor changes to Image* vars

--- server/protogame_server.c
+++ server/protogame_server.c
@@ -28,6 +28,8 @@
 // world
 World server_world;
 struct timeval world_update_time;
+Image* surface_elevation;
+Image* surface_texture;
 // flags
 int connectivity = 1;
 int exchange_update = 1;
@@ -959,19 +961,21 @@ int main(int argc, char** argv) {
   // load the images
   fprintf(stdout, "[Main] loading elevation image from %s ... ",
           elevation_filename);
-  Image* surface_elevation = Image_load(elevation_filename);
+  surface_elevation = Image_load(elevation_filename);
   if (surface_elevation) {
     fprintf(stdout, "Done! \n");
   } else {
     fprintf(stdout, "Fail! \n");
+    return -1;
   }
   fprintf(stdout, "[Main] loading texture image from %s ... ",
           texture_filename);
-  Image* surface_texture = Image_load(texture_filename);
+  surface_texture = Image_load(texture_filename);
   if (surface_texture) {
     fprintf(stdout, "Done! \n");
   } else {
     fprintf(stdout, "Fail! \n");
+    return -1;
   }
 
 #ifdef _USE_SERVER_SIDE_FOG_
