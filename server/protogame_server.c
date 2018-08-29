ddd77232c9e461b34e8a76422f15605fec9d210b
Leonardo Sarra <leonardosarra@outlook.com>
2018-08-29 21:28:01 +0200

    Server: Don't send messages to client that are not inside the server world

--- server/protogame_server.c
+++ server/protogame_server.c
@@ -159,7 +159,7 @@ int UDPHandler(int socket_udp, char* buf_rcv, struct sockaddr_in client_addr) {
       pthread_mutex_lock(&users_mutex);
       MessageListItem* mli = (MessageListItem*)malloc(sizeof(MessageListItem));
       ClientListItem* user = ClientList_findByID(users, mp->message.id);
-      if (user == NULL || !user->inside_chat) {
+      if (user == NULL || !user->inside_chat || !user->inside_world) {
         free(mli);
         pthread_mutex_unlock(&users_mutex);
         return 0;
@@ -575,7 +575,7 @@ int sendMessages(int socket_udp) {
   pthread_mutex_lock(&users_mutex);
   ClientListItem* client = users->first;
   for (; client != NULL; client = client->next) {
-    if (!client->is_udp_addr_ready || !client->inside_chat) continue;
+    if (!client->is_udp_addr_ready || !client->inside_chat || !client->inside_world) continue;
     int ret = sendto(socket_udp, buf_send, size, 0,
                      (struct sockaddr*)&client->user_addr_udp,
                      (socklen_t)sizeof(client->user_addr_udp));
