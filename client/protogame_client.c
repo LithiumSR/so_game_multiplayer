36fad50157c09cfc55b55b9b4b577f4bd8a1c375
Leonardo Sarra <leonardosarra@outlook.com>
2018-09-01 14:29:32 +0200

    Client: Improved goodbye message

--- client/protogame_client.c
+++ client/protogame_client.c
@@ -28,7 +28,7 @@
 int window;
 Vehicle* vehicle;  // The vehicle
 int id;
-char username[32];
+char username[USERNAME_LEN];
 AudioContext* background_track = NULL;
 // flags and counters
 char connectivity = 1;
@@ -140,7 +140,7 @@ int sendUpdates(int socket_udp, struct sockaddr_in server_addr, int serverlen) {
     connectivity = 0;
     exchange_update = 0;
     fprintf(stderr,
-            "[WARNING] Server is not avaiable. Terminating the client now...");
+            "\n[WARNING] Server is not avaiable. Terminating the client now...");
     WorldViewer_exit(0);
   } else if (last_update_time.tv_sec != -1 &&
              current_time.tv_sec - last_update_time.tv_sec >
@@ -148,7 +148,7 @@ int sendUpdates(int socket_udp, struct sockaddr_in server_addr, int serverlen) {
     connectivity = 0;
     exchange_update = 0;
     fprintf(stderr,
-            "[WARNING] Server is not avaiable. Terminating the client now...");
+            "\n[WARNING] Server is not avaiable. Terminating the client now...");
     WorldViewer_exit(0);
   }
   pthread_mutex_unlock(&time_lock);
@@ -242,14 +242,14 @@ void* UDPReceiver(void* args) {
     // debug_print("[UDP_Receiver] Received %d bytes over UDP\n", bytes_read);
     PacketHeader* ph = (PacketHeader*)buf_rcv;
     if (ph->size != bytes_read) {
-      debug_print("[WARNING] Skipping partial UDP packet \n");
+      debug_print("\n[WARNING] Skipping partial UDP packet \n");
       usleep(RECEIVER_SLEEP);
       continue;
     }
     switch (ph->type) {
       case (PostDisconnect): {
         fprintf(stderr,
-                "[WARNING] You were kicked out of the server for inactivity... "
+                "\n[WARNING] You were kicked out of the server for inactivity... "
                 "Closing the client now \n");
         sendGoodbye(socket_desc, id);
         connectivity = 0;
