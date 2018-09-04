c1177d950e69951a950e42d55429cfe05fce0ba5
Leonardo Sarra <leonardosarra@outlook.com>
2018-09-04 14:33:07 +0200

    Tests: Improved client list tests

--- tests/test_client_list.c
+++ tests/test_client_list.c
@@ -6,6 +6,7 @@
 #include "../game_framework/protogame_protocol.h"
 int main(int argc, char const* argv[]) {
   printf("Starting client list tests...");
+  char flag = 0;
   ClientListHead* test = (ClientListHead*)malloc(sizeof(ClientListHead));
   ClientList_print(test);
 
@@ -25,30 +26,52 @@ int main(int argc, char const* argv[]) {
   printf("Finding and comparing elements with id 2...");
   if (cil == NULL) {
     printf("ERROR IN DETACH \n");
-    return -1;
+    flag = -1;
   }
   if (cil == u2) printf("OK \n");
   ClientListItem* cil2 = ClientList_findByID(test, 1);
   printf("Finding and comparing elements with id 1...");
   if (cil2 == NULL) {
     printf("ERROR IN DETACH \n");
-    return -1;
+    flag = -1;
   }
   if (cil2 == u1) printf("OK \n ");
   printf("Removing elements with id 2...");
-  ClientList_detach(test, cil2);
+  ClientList_detach(test, u2);
+  ClientListItem* remove2 = ClientList_findByID(test, 2);
+  if (remove2 != NULL){
+    printf("ERROR %d IN DETACH \n",remove2->id);
+    flag = -1;
+  }
+
   printf("Done.\n");
   ClientList_print(test);
   printf("Removing elements with id 1...");
-  ClientList_detach(test, cil);
+  ClientList_detach(test, u1);
+  ClientListItem* remove1 = ClientList_findByID(test, 1);
+  if (remove1 != NULL){
+    printf("ERROR IN DETACH \n");
+    flag = -1;
+  }
   printf("Done.\n");
   ClientList_print(test);
   printf("Readding elements previously removed...");
   ClientList_insert(test, u1);
+  ClientListItem* readd1 = ClientList_findByID(test, 1);
+  if (readd1 == NULL) {
+    printf("ERROR IN READD \n");
+    flag = -1;
+  }
   ClientList_insert(test, u2);
+  ClientListItem* readd2 = ClientList_findByID(test, 2);
+  if (readd2 == NULL) {
+    printf("ERROR IN READD \n");
+    flag = -1;
+  }
   printf("Done.\n");
   printf("Destroying resources for good...");
   ClientList_destroy(test);
   printf("Done.\n");
   fflush(stdout);
+  return flag;
 }
